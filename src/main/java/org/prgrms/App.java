03d81b1cf690e6a42f407213dbc5555a97058138
kwhyo <rkgmlgygml@naver.com>
2021-08-16 19:06:46 +0900

    찐 문제아

--- src/main/java/org/prgrms/App.java
+++ src/main/java/org/prgrms/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println("와 됐당~");
 
         System.out.println( "워떤 기능을 추가" );
-        System.out.println( "아 근데 바꿀거임 새기능" );
+        System.out.println( "그래 바꿔버령" );
         System.out.println( "또 다른 특별한 기능을 추가" );
     }
 }
