a405cd70964d0eafa7dc78a3a019693d41e7e81f
kwhyo <rkgmlgygml@naver.com>
2021-08-16 18:57:13 +0900

    새 기능 추가 함

--- src/main/java/org/prgrms/App.java
+++ src/main/java/org/prgrms/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "깃 연결 완료!" );
         System.out.println("와 됐당~");
+
+        System.out.println( "워떤 기능을 추가" );
     }
 }
